8056b2ae2b3a23d7f3f0b63b8bf3c63210421de9
majunliang <majunliang@dongpi.com>
2016-05-13 17:21:32 +0800

    添加

--- Examples/SDWebImage Demo/MasterViewController.m
+++ Examples/SDWebImage Demo/MasterViewController.m
@@ -6,9 +6,9 @@
 //  Copyright (c) 2012 Dailymotion. All rights reserved.
 //
 
+#import "DetailViewController.h"
 #import "MasterViewController.h"
 #import <SDWebImage/UIImageView+WebCache.h>
-#import "DetailViewController.h"
 
 @interface MasterViewController () {
     NSMutableArray *_objects;
@@ -22,30 +22,28 @@
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self)
-    {
+    if (self) {
         self.title = @"SDWebImage";
-        self.navigationItem.rightBarButtonItem = [UIBarButtonItem.alloc initWithTitle:@"Clear Cache"
-                                                                                style:UIBarButtonItemStylePlain
-                                                                               target:self
-                                                                               action:@selector(flushCache)];
-        
+        self.navigationItem.rightBarButtonItem =
+            [UIBarButtonItem.alloc initWithTitle:@"Clear Cache" style:UIBarButtonItemStylePlain target:self action:@selector(flushCache)];
+
         // HTTP NTLM auth example
         // Add your NTLM image url to the array below and replace the credentials
+        // TODO:read
         [SDWebImageManager sharedManager].imageDownloader.username = @"httpwatch";
         [SDWebImageManager sharedManager].imageDownloader.password = @"httpwatch01";
-        
-        _objects = [NSMutableArray arrayWithObjects:
-                    @"http://www.httpwatch.com/httpgallery/authentication/authenticatedimage/default.aspx?0.35786508303135633",     // requires HTTP auth, used to demo the NTLM auth
-                    @"http://assets.sbnation.com/assets/2512203/dogflops.gif",
-                    @"http://www.ioncannon.net/wp-content/uploads/2011/06/test2.webp",
-                    @"http://www.ioncannon.net/wp-content/uploads/2011/06/test9.webp",
-                    nil];
 
-        for (int i=0; i<100; i++) {
+        _objects
+            =
+                [NSMutableArray
+                    arrayWithObjects:@"http://www.httpwatch.com/httpgallery/authentication/authenticatedimage/default.aspx?0.35786508303135633",     // requires HTTP auth, used to demo the NTLM auth
+                                     @"http://assets.sbnation.com/assets/2512203/dogflops.gif",
+                                     @"http://www.ioncannon.net/wp-content/uploads/2011/06/test2.webp",
+                                     @"http://www.ioncannon.net/wp-content/uploads/2011/06/test9.webp", nil];
+
+        for (int i = 0; i < 100; i++) {
             [_objects addObject:[NSString stringWithFormat:@"https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage%03d.jpg", i]];
         }
-
     }
     [SDWebImageManager.sharedManager.imageDownloader setValue:@"SDWebImage Demo" forHTTPHeaderField:@"AppName"];
     SDWebImageManager.sharedManager.imageDownloader.executionOrder = SDWebImageDownloaderLIFOExecutionOrder;
@@ -57,7 +55,7 @@
     [SDWebImageManager.sharedManager.imageCache clearMemory];
     [SDWebImageManager.sharedManager.imageCache clearDisk];
 }
-							
+
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
@@ -78,10 +76,9 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *CellIdentifier = @"Cell";
-    
+
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil)
-    {
+    if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
     }
 
@@ -91,14 +88,14 @@
     cell.textLabel.text = [NSString stringWithFormat:@"Image #%ld", (long)indexPath.row];
     cell.imageView.contentMode = UIViewContentModeScaleAspectFill;
     [cell.imageView sd_setImageWithURL:[NSURL URLWithString:[_objects objectAtIndex:indexPath.row]]
-                      placeholderImage:[UIImage imageNamed:@"placeholder"] options:indexPath.row == 0 ? SDWebImageRefreshCached : 0];
+                      placeholderImage:[UIImage imageNamed:@"placeholder"]
+                               options:indexPath.row == 0 ? SDWebImageRefreshCached : 0];
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    if (!self.detailViewController)
-    {
+    if (!self.detailViewController) {
         self.detailViewController = [[DetailViewController alloc] initWithNibName:@"DetailViewController" bundle:nil];
     }
     NSString *largeImageURL = [[_objects objectAtIndex:indexPath.row] stringByReplacingOccurrencesOfString:@"small" withString:@"source"];
